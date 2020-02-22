cbf0066879b5183e52fb07c63796328c79142f05
mrBellwood <kristian.w.finstad@gmail.com>
2020-02-22 16:27:11 +0100

    User interface update

---
+++ EmailGUI/Frames/MainKeyInvalid.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+// applib
+using AppLib.Collection;
+
+namespace EmailGUI.Frames
+{
+    /// <summary>
+    /// Interaction logic for MainKeyInvalid.xaml
+    /// </summary>
+    public partial class MainKeyInvalid : Page
+    {
+        public MainKeyInvalid(bool expired)
+        {
+            InitializeComponent();
+
+            if (expired)
+            {
+                msg1.Text = "Prøvetiden har utløpt.";
+                msg2.Text = "Kontakt utvikler.";
+            }
+            else
+            {
+                string logPath = GetPath.Log;
+                msg1.Text = "Sikkerhetsfeil! Nøkkelfilen mangler integritet.";
+                msg2.Text = "Feil registert i log.";
+            }
+        }
+
+        public void QuitApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
--- EmailGUI/MainWindow.xaml.cs
+++ EmailGUI/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+// own lib namespaces here
+using AppLib;
+using AppLib.Collection;
+using AppLib.Secrets;
 
 namespace EmailGUI
 {
@@ -22,7 +26,36 @@ namespace EmailGUI
     {
         public MainWindow()
         {
+            string keyPaht = GetPath.Keys;      // get key path
+
+            // CreateKeyFile createKey = new CreateKeyFile(2020, 07, 31);      // delete this before production or not needed
+            // createKey.WriteKeyFile(keyPaht);                                // delete this before production or not needed
+
+            KeyFile keyFile = new KeyFile();    // get keyfile content
+           
             InitializeComponent();
+
+            LoadByValidKey(keyFile);    // load page dependent on keyFile
         }
+
+
+        public void LoadByValidKey(KeyFile keyFileInput)
+        {
+            /** Load page dependend on valid key file */
+
+            VerifyKeyFile verifyKey = new VerifyKeyFile(keyFileInput);  // verify key
+            
+            if (!verifyKey.KeyFileValid)    // if keyfile not valid
+            {
+                // load invalid screen, boolean argument for expired key file
+                Main.Content = new Frames.MainKeyInvalid(verifyKey.DateExpired);
+            }
+            else
+            {
+                Main.Content = new Frames.MainKeyValid();
+            }
+
+        }
+
     }
 }
